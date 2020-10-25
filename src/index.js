73bbb06bd99d7d70f4d076a69931a20b802f831f
FokinAlexey <70858193+FokinAlexey@users.noreply.github.com>
2020-10-25 16:24:42 +0300

    fix: add solution

--- src/index.js
+++ src/index.js
@@ -1,19 +1,21 @@
 module.exports = function toReadable (n) { // n === number in the beginning
-
-
-    var c = n%100, b = n%10, a = (n-b)/10, // n = 100*c + 10*a + b
-        A1 = ['zero','one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'],
-        A2 = ['eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen',
-               'sixteen', 'seventeen', 'eighteen', 'nineteen'],
-        A3 = ['ten', 'twenty', 'thirty', 'forty', 'fifty',
-              'sixty', 'seventy', 'eighty', 'ninety'];        
-    if (n<10) return A1[n];          
-    if (n > 10 && n < 20) return A2[n-11];             
-    if (b === 0) return A3[a-1];
-
-    return A1(Math.trunc(c))+' hundred '+A2(Math.trunc(b-c*10))+' 'A1(n-b*10);
-
-    //return A3[a-1]+' '+A1[b-1];
-
-
+ let b = n % 10, a = (n - b) / 10,
+     d = n % 100, c = (n - d) / 100,
+     e = ((n - b) % 100) / 10,
+            
+     A1 = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'],
+     A2 = ['ten','twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    
+   if (n == 0) return 'zero';
+   if (n < 20) return A1[n - 1];
+   if (n < 100) {
+   if (b === 0) return A2[a - 1];
+       return A2[a - 1] + ' ' + A1[b - 1];
+ }
+   if (n < 1000) {
+   if (d === 0) return A1[c - 1]+ ' hundred';
+   if (b === 0) return A1[c - 1] + ' hundred ' + A2[e - 1];
+   if (n % 100 < 20) return A1[c - 1] + ' hundred ' + A1[d - 1];
+   return A1[c - 1] + ' hundred ' + A2[e - 1] + ' ' + A1[b - 1];
+ } 
 }
